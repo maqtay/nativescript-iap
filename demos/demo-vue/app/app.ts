a9ab044a271ece4dd10051839291d288591ac4c0
Murat Aktay <32091847+maqtay@users.noreply.github.com>
2023-09-17 18:38:47 +0300

    Delete demos/demo-vue directory

--- demos/demo-vue/app/app.ts
+++
@@ -1,9 +0,0 @@
-import Vue from "nativescript-vue";
-import App from "@/components/App.vue";
-
-// Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = !__DEV__;
-
-new Vue({
-    render: h => h("frame", [h(App)])
-}).$start();
--- demos/demo-vue/webpack.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const webpack = require("@nativescript/webpack");
-
-module.exports = (env) => {
-    webpack.init(env);
-    webpack.useConfig("vue");
-
-    return webpack.resolveConfig();
-};
